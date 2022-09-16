d84219a16d54e6592bf4fe5267f2476f903d9a1e
Gokul Narayanan <gokul.narayanan@microchip.com>
2022-09-16 11:23:56 +0530

    - Optimised Transmit and Receive Functions in a single   SENT_TransmitReceive() - Handled all Blocking calls with Non-Blocking ones

--- dspic33ck-curiosity-sent-loopback-demo.X/main.c
+++ dspic33ck-curiosity-sent-loopback-demo.X/main.c
@@ -22,6 +22,7 @@
 #include <stdbool.h>
 #include <libpic30.h>
 #include <xc.h>
+#include <p33CK256MP508.h>
 
 #include "mcc_generated_files/system/system.h"
 #include "mcc_generated_files/system/pins.h"
@@ -33,36 +34,43 @@
 
 const volatile struct SENT_TRANSMIT_INTERFACE *sentTx = &SENT_Transmitter;
 const volatile struct SENT_RECEIVE_INTERFACE *sentRx = &SENT_Receiver;
+
+//Functions
+void SENT_TxRxInitialization();
+void SENT_TxRxDeInitialization();
+bool SENT_TransmitReceive();
+bool SENT_WaitForTransmissionComplete(bool *result);
+bool SENT_WaitForReceptionComplete(bool *result);
+bool compare(struct SENT_DATA_TRANSMIT *txdata, struct SENT_DATA_RECEIVE *rxdata);
+bool waitForCallbackTimeout(bool (*handler)(bool *status), uint32_t timeOut);
+
+static bool softTimeOut(uint32_t intVal);
+static bool (*callbackResponseWaitHandler)(bool *status) = NULL;
+
+//Enums
+enum SENT_STATE{
+    SENT_START_STATE = 0,
+    SENT_ASYNC_STATE = 1,
+    SENT_SYNC_STATE = 2,
+    SENT_DEINIT_STATE = 3,
+    SENT_EXIT_STATE = 4,
+};
+
 /*
     Main application
 */
-
-void SENT_TxRxInitialization();
-void SENT_TxRxDeInitialization();
-bool SENT_AsynchronousTxandRx();
-bool SENT_SynchronousTxandRx();
-bool compare(struct SENT_DATA_TRANSMIT *txdata, struct SENT_DATA_RECEIVE *rxdata);
-
-enum SENT_STATE{
-    SENT_START_STATE = 0,
-    SENT_ASYNC_STATE,
-    SENT_SYNC_STATE,
-    SENT_DEINIT_STATE,
-    SENT_EXIT_STATE,
-};
-
 int main(void)
 {
     bool sent_status = false;
     uint8_t current_state = (uint8_t)SENT_START_STATE;
     SYSTEM_Initialize();
 
-    
     while(1)
     {
         switch(current_state) 
        {    
             case SENT_START_STATE:
+                //Start State
                 printf("#########################################\r\n");
                 printf("           SENT LOOPBACK DEMO            \r\n");
                 printf("#########################################\r\n");
@@ -70,29 +78,38 @@ int main(void)
                 current_state = SENT_ASYNC_STATE;
                 break;
             case SENT_ASYNC_STATE:
-               sent_status = SENT_AsynchronousTxandRx();
+                //Async State
+               sentTx->TransmitModeSet(SENT_TRANSMIT_ASYNCHRONOUS);  //Set Transmit Mode to ASYNCHRONOUS
+               sent_status = SENT_TransmitReceive(); //SENT Transmit & Receive 
                sent_status ? UserLEDGreen_SetHigh() : UserLEDRed_SetHigh();
-               sent_status ? printf("SENT Asynchronous Success\r\n") : printf("SENT Asynchronous Unsuccessful\r\n");
+               sent_status ? printf("SENT Asynchronous Communication Success\r\n") : printf("SENT Asynchronous Communication Unsuccessful\r\n"); 
                printf("------------------------------------------\r\n\r\n");
                current_state = SENT_SYNC_STATE;
                sent_status = false;             
                break;
            case SENT_SYNC_STATE:
-               sent_status = SENT_SynchronousTxandRx();
+               //Sync State
+               sentTx->TransmitModeSet(SENT_TRANSMIT_SYNCHRONOUS); //Set Transmit Mode to SYNCHRONOUS
+               sent_status = SENT_TransmitReceive(); //SENT Transmit & Receive 
                sent_status ? UserLEDGreen_SetHigh() : UserLEDRed_SetHigh();
-               sent_status ? printf("SENT Synchronous Success\r\n") : printf("SENT Synchronous Unsuccessful\r\n");
+               sent_status ? printf("SENT Synchronous Communication Success\r\n") : printf("SENT Synchronous Communication Unsuccessful\r\n");
                printf("------------------------------------------\r\n\r\n");
                current_state = SENT_DEINIT_STATE;
                sent_status = false;
                break;
            case SENT_DEINIT_STATE:
+               //Deinit State
                SENT_TxRxDeInitialization();
                current_state = SENT_EXIT_STATE;
-               printf("SENT DeInitialization Successful\r\n");
+               printf("SENT De-Initialization Successful\r\n");
                printf("#########################################\r\n");
                break;
             case SENT_EXIT_STATE:
+                //Exit State
                 break;    
+            default:
+                //Default State
+                break;
        }
     }    
 }
@@ -143,9 +160,12 @@ bool compare(struct SENT_DATA_TRANSMIT *txdata, struct SENT_DATA_RECEIVE *rxdata
     return status;
 }
 
-bool SENT_AsynchronousTxandRx()
+bool SENT_TransmitReceive()
 {
     bool result = false;
+    bool txCompleteFlag = false;
+    bool rxCompleteFlag = false;
+    
     struct SENT_DATA_TRANSMIT sentDataTransmit;
     struct SENT_DATA_RECEIVE sentDataReceive;
 
@@ -159,37 +179,82 @@ bool SENT_AsynchronousTxandRx()
 
     sentTx->Transmit(&sentDataTransmit);
     
-    while (sentTx->IsTransmissionComplete() == false);
-    while (sentRx->IsDataReceived() == false);
-
-    sentDataReceive = sentRx->Receive();
-    result = compare(&sentDataTransmit, &sentDataReceive);
+    txCompleteFlag = waitForCallbackTimeout(&SENT_WaitForTransmissionComplete,2000);
+    rxCompleteFlag = waitForCallbackTimeout(&SENT_WaitForReceptionComplete,2000);
+    
+    __delay32(1000);
+    
+    if((txCompleteFlag & rxCompleteFlag) == true)
+    {
+        sentDataReceive = sentRx->Receive();
+        result = compare(&sentDataTransmit, &sentDataReceive);
+    
+        return result;
+    }
 
     return result;
 }
 
-bool SENT_SynchronousTxandRx() 
+bool SENT_WaitForTransmissionComplete(bool *result)
 {
-    bool result = false;
-    struct SENT_DATA_TRANSMIT sentDataTransmit;
-    struct SENT_DATA_RECEIVE sentDataReceive;
+    if(sentTx->IsTransmissionComplete() == true)
+    {
+        *result = true;
+        return true;
+    }
+    return false;
+}
 
-    sentDataTransmit.status = 2;
-    sentDataTransmit.data1 = 1;
-    sentDataTransmit.data2 = 2;
-    sentDataTransmit.data3 = 3;
-    sentDataTransmit.data4 = 4;
-    sentDataTransmit.data5 = 5;
-    sentDataTransmit.data6 = 6;
+bool SENT_WaitForReceptionComplete(bool *result)
+{
+    if(sentRx->IsDataReceived() == true)
+    {
+        *result = true;
+        return true;
+    }
+    return false;
+}
 
-    sentTx->TransmitModeSet(SENT_TRANSMIT_SYNCHRONOUS);
-    sentTx->Transmit(&sentDataTransmit);
-    while (sentTx->IsTransmissionComplete() == false);
-    while (sentRx->IsDataReceived() == false);
+static bool softTimeOut(uint32_t intVal)
+{
+    static uint32_t count = 0;
     
-    sentDataReceive = sentRx->Receive();
-    result = compare(&sentDataTransmit, &sentDataReceive);
+    if(count >= intVal)
+    {
+        count = 0;
+        return true;
+    }
     
-    return result;
+    count++;
+    return false;
+}
 
+bool waitForCallbackTimeout(bool (*handler)(bool *status), uint32_t timeOut)
+{
+    callbackResponseWaitHandler = handler;
+    bool res = false;
+    
+    while(1) 
+    {
+        if(handler != NULL) {
+            if(handler(&res) == true)
+            {
+                res = true;
+                break;
+            }
+        } 
+        else 
+        {
+            printf("[!] Did not registered test callback function...\r\n");
+            break;
+        }
+
+        if(softTimeOut(timeOut) == true)
+        {
+            printf("[!] Timeout occurred!.Check your loopback connection.\r\n");
+            break;
+        }
+    }
+
+    return res;
 }
